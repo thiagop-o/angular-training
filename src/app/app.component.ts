62d0be8d819d39d07dcba6ae9d609cf25a89e877
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-21 22:24:15 -0300

    Dia 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-first-project';
+export class AppComponent implements OnInit{
+  count = 0;
+  text = '';
+  pessoas = [{
+    nome: "Thiago",
+    sobrenome: "Oliveira"
+  },{
+    nome: "Maria",
+    sobrenome: "Silva"
+  },{
+    nome: "Joao",
+    sobrenome: "Pereira"
+  },{
+    nome: "Marcio",
+    sobrenome: "Santos"
+  }];
+
+  constructor (){}
+
+  ngOnInit(){
+    console.log(this.pessoas)
+    let interval = setInterval(() => {
+      this.count++;
+      if (this.count === 10){
+        clearInterval(interval)
+      }
+    },1000)
+  }
+
+  clicou(): void{
+    console.log("foi clicado");
+  }
+
 }
